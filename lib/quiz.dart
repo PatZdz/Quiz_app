f04f45d625b05d2b9d01309ae126e0416a841bf8
Daniel <d.semerjyan@gmail.com>
2024-10-09 19:15:37 +0200

    change color

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -69,7 +69,7 @@ class _QuizState extends State<Quiz> {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Colors.deepPurple,
+                Colors.deepGreen,
                 Colors.purple,
               ],
               begin: Alignment.topLeft,
